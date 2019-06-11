861860e4a755e0056b7a7775c78873de37268b96
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:44:53 +0300

    Update HomeRepository.php

--- app/Repositories/HomeRepository.php
+++ app/Repositories/HomeRepository.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace App\Repositories;
-
 use App\Product;
 
 class HomeRepository
 {
-    public function __construct(Product $product){
+    public function __construct(Product $product)
+    {
       $this->product = $product;
       }
-
     public function getProduct()
     {
       return $this->product->take(8)->get();
