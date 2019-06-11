3b4e14e59e6031e9429bd3be4852b9cb01f5c388
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 09:03:18 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,38 +4,25 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
-
 Route::get('/', 'HomeController@index');
-
 Route::get('/shop', 'ShopController@index');
 Route::get('/shop/search', 'ShopController@search');
 Route::get('/category/{shop}', 'ShopController@category');
-
 Route::get('/product/review','ReviewController@index');
 Route::get('/product/{product}', 'ProductController@show');
 Route::get('/product/comment','CommentController@index');
 Route::get('/users','UserController@index');
-
 Route::post('/product/review/add','ReviewController@store');
 Route::post('/product/comment/add','CommentController@store');
 Route::post('/product/review/add','ReviewController@store');
-
 Route::get('cart','CartController@index');
 Route::post('cart/add','CartController@store');
 Route::delete('/cart/delete/{id}', 'CartController@delete');
-
 Route::get('/wishlist','WishlistController@index');
 Route::post('/wishlist/add','WishlistController@store');
 Route::delete('/wishlist/delete/{id}', 'WishlistController@delete');
-
 Route::get('/order','OrderController@index');
 Route::post('/order/add','OrderController@store');
-
 Route::get('/checkout','CheckoutController@index');
 Route::post('/checkout/add','CheckoutController@store');
-
 Route::post('/contact/add','ContactController@store');
-
-Route::group(['middleware' => 'auth:api'], function(){
-
-    });
