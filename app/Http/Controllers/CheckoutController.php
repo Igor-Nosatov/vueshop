725eacc8cfa585643686a501ff886eabeec62096
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:49:48 +0300

    Update CheckoutController.php

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Repositories\CheckoutRepository;
 
 class CheckoutController extends Controller
 {
-
     public function __construct(CheckoutRepository $checkout)
     {
         $this->checkout = $checkout;
     }
-
     public function store(Request $request)
     {
       $this->checkout->createCheckout($request->all());
