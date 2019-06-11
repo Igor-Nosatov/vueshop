471db2f75da4f12f711224f0c622f54e3c732857
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:36:40 +0300

    Update Review.php

--- app/Review.php
+++ app/Review.php
@@ -1,14 +1,11 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
   protected $fillable = ['star', 'name',
       'email', 'phone', 'message', 'product_id', 'user_id'];
-
   public function product()
   {
     return $this->belongsTo('App\Product', 'product_id');
