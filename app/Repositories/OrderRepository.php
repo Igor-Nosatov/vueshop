5e8c065a2e111611f89afc2761d4b5e925e79728
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:45:22 +0300

    Update OrderRepository.php

--- app/Repositories/OrderRepository.php
+++ app/Repositories/OrderRepository.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App\Repositories;
-
 use App\Order;
 
 class OrderRepository
 {
     protected $order;
-
     public function __construct(Order $order)
     {
         $this->order = $order;
@@ -16,14 +13,12 @@ class OrderRepository
     {
         return $this->order->get();
     }
-
     public function createOrder(array $data) : Order
-	  {
+	{
 		    return $this->order->create($data);
   	}
-
     public function deleteOrder($id) : bool
-	  {
+	{
 		    return $this->order->find($id)->delete();
   	}
 }
