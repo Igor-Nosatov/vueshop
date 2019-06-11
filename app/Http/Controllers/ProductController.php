0f7f73958e9be1891408f48ea74d5156403d9a1d
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:57:43 +0300

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,26 +1,22 @@
 <?php
+namespace App\Http\Controllers;
+use App\Product;
+use App\Comment;
+use App\Review;
+use Auth;
+use Illuminate\Http\Request;
 
-    namespace App\Http\Controllers;
-
-    use App\Product;
-    use App\Comment;
-    use App\Review;
-    use Auth;
-
-    use Illuminate\Http\Request;
-
-    class ProductController extends Controller
+class ProductController extends Controller {
+    
+    public function index() 
     {
-        public function index()
-        {
-            return response()->json(Product::all(),200);
-        }
-
-        public function show($id)
-        {
-            $data['product'] = Product::query()->with('category')->find($id);
-            $data['comment'] = Comment::query()->where('product_id', '=', $id)->limit(5)->get();
-            $data['review'] = Review::query()->where('product_id', '=', $id)->get();
-            return response()->json($data,200);
-        }
+        return response()->json(Product::all(), 200);
     }
+    public function show($id) 
+    {
+        $data['product'] = Product::query()->with('category')->find($id);
+        $data['comment'] = Comment::query()->where('product_id', '=', $id)->limit(5)->get();
+        $data['review'] = Review::query()->where('product_id', '=', $id)->get();
+        return response()->json($data, 200);
+    }
+}
