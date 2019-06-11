f9dedf656f84d72a9a5053aadc6309bb8a6ccf54
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:46:53 +0300

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Repositories\CartRepository;
 
@@ -11,19 +9,16 @@ class CartController extends Controller
     {
         $this->cart = $cart;
     }
-
     public function index()
     {
         $data['cart'] = $this->cart->getCart();
         return response()->json($data, 200);
     }
-
     public function store(Request $request)
     {
       $this->cart->createCart($request->all());
       return response()->json('success');
     }
-
     public function delete($id)
     {
         $cart = $this->cart->deleteCart($id);
