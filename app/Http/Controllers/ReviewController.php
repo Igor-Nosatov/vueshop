f1b681fee8e17114673ccfd1d47e061479c36615
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:58:15 +0300

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -1,25 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Review;
 use Illuminate\Http\Request;
 
-class ReviewController extends Controller
-{
-  public function store(Request $request)
- {
-   $review = new Review([
-    'star' => $request->get('star'),
-    'name' => $request->get('name'),
-    'email' => $request->get('email'),
-    'phone' => $request->get('phone'),
-    'message' => $request->get('message'),
-    'product_id' => $request->get('product_id')
-  ]);
-
-  $review->save();
-
-  return response()->json('success');
- }
+class ReviewController extends Controller {
+    
+    public function store(Request $request) {
+        $review = new Review(['star' => $request->get('star'), 'name' => $request->get('name'), 'email' => $request->get('email'), 'phone' => $request->get('phone'), 'message' => $request->get('message'), 'product_id' => $request->get('product_id') ]);
+        $review->save();
+        return response()->json('success');
+    }
 }
