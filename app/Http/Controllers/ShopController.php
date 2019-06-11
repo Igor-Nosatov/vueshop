67a82a108ff283f6e3b2a7a1cd2e9dd4cf3e9473
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 09:01:17 +0300

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -1,49 +1,45 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Repositories\ProductRepository;
 use App\Repositories\ColorRepository;
 use App\Repositories\BrandRepository;
 use App\Repositories\CategoryRepository;
 use Illuminate\Http\Request;
 
-class ShopController extends Controller
-{
-	protected $product;
-	protected $color;
-	protected $brand;
-	protected $category;
-
-	public function __construct(ProductRepository $product, ColorRepository $color, BrandRepository $brand, CategoryRepository $category)
-	{
-			$this->product = $product;
-			$this->color = $color;
-			$this->brand = $brand;
-			$this->category = $category;
-	}
-
-	public function index()
-	{
-		$data['products'] = $this->product->getProduct();
-		$data['colors'] = $this->color->getColor();
-		$data['brands'] = $this->brand->getBrand();
-		$data['categories'] = $this->category->getCategory();
-		return response()->json($data, 200);
-	}
-	public function search(){
-		$data['products'] = $this->product->getProduct();
-		$data['colors'] = $this->color->getColor();
-		$data['brands'] = $this->brand->getBrand();
-		$data['categories'] = $this->category->getCategory();
-		return response()->json($data, 200);
-	}
-	public function category($id)
-	{
-		$data['products'] = $this->product->getByIdProduct($id);
-		$data['colors'] = $this->color->getColor();
-		$data['brands'] = $this->brand->getBrand();
-		$data['categories'] = $this->category->getCategory();
-		return response()->json($data, 200);
-	}
+class ShopController extends Controller {
+    protected $product;
+    protected $color;
+    protected $brand;
+    protected $category;
+    public function __construct(ProductRepository $product, ColorRepository $color, BrandRepository $brand, CategoryRepository $category) 
+    {
+        $this->product = $product;
+        $this->color = $color;
+        $this->brand = $brand;
+        $this->category = $category;
+    }
+    public function index() 
+    {
+        $data['products'] = $this->product->getProduct();
+        $data['colors'] = $this->color->getColor();
+        $data['brands'] = $this->brand->getBrand();
+        $data['categories'] = $this->category->getCategory();
+        return response()->json($data, 200);
+    }
+    public function search() 
+    {
+        $data['products'] = $this->product->getProduct();
+        $data['colors'] = $this->color->getColor();
+        $data['brands'] = $this->brand->getBrand();
+        $data['categories'] = $this->category->getCategory();
+        return response()->json($data, 200);
+    }
+    public function category($id) 
+    {
+        $data['products'] = $this->product->getByIdProduct($id);
+        $data['colors'] = $this->color->getColor();
+        $data['brands'] = $this->brand->getBrand();
+        $data['categories'] = $this->category->getCategory();
+        return response()->json($data, 200);
+    }
 }
