d17e0f26e47a5bb3348b5cdea0e2b71a0b33d3f4
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:46:24 +0300

    Update WishlistRepository.php

--- app/Repositories/WishlistRepository.php
+++ app/Repositories/WishlistRepository.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Repositories;
-
 use App\Wishlist;
 
 class WishlistRepository
 {
-
   public function __construct(Wishlist $wishlist)
   {
       $this->wishlist = $wishlist;
@@ -15,12 +12,10 @@ class WishlistRepository
   {
       return $this->wishlist->get();
   }
-
   public function createWishlist(array $data) : Wishlist
   {
       return $this->wishlist->create($data);
   }
-
   public function deleteWishlist($id) : bool
   {
       return $this->wishlist->find($id)->delete();
