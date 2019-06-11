a1773e395f5b3309b55c24092997f166f27e8d14
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:45:45 +0300

    Update ProductRepository.php

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -1,15 +1,13 @@
 <?php
-
 namespace App\Repositories;
-
 use App\Product;
 
 class ProductRepository
 {
-  public function __construct(Product $product){
+  public function __construct(Product $product)
+  {
     $this->product = $product;
-    }
-
+  }
   public function getProduct()
   {
     return $this->product->get();
