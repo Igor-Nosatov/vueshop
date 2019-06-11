5d935b90c6ea033ce02c673547d6f8d240691659
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 09:02:31 +0300

    Update WishlistController.php

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -1,32 +1,22 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Repositories\WishlistRepository;
 
-class WishlistController extends Controller
-{
-	public function __construct(WishlistRepository $wishlist)
-  {
-      $this->wishlist = $wishlist;
-  }
-
-	public function index()
-	{
-			$data['wishlist'] = $this->wishlist->getWishlist();
-			return response()->json($data, 200);
-	}
-
-	public function store(Request $request)
-	{
-		$this->wishlist->createWishlist($request->all());
-		return response()->json('success');
-	}
-
-	public function delete($id)
-	{
-			$wishlist = $this->wishlist->deleteWishlist($id);
-			return response()->json('success');
-	}
+class WishlistController extends Controller {
+    public function __construct(WishlistRepository $wishlist) {
+        $this->wishlist = $wishlist;
+    }
+    public function index() {
+        $data['wishlist'] = $this->wishlist->getWishlist();
+        return response()->json($data, 200);
+    }
+    public function store(Request $request) {
+        $this->wishlist->createWishlist($request->all());
+        return response()->json('success');
+    }
+    public function delete($id) {
+        $wishlist = $this->wishlist->deleteWishlist($id);
+        return response()->json('success');
+    }
 }
