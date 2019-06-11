c236a6efdae5735e1fead6840389dfd80fcfb70d
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:32:36 +0300

    Update Comment.php

--- app/Comment.php
+++ app/Comment.php
@@ -1,13 +1,10 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
   protected $fillable = ['name', 'email', 'phone', 'message', 'product_id'];
-
   public function product()
   {
     return $this->belongsTo('App\Product');
