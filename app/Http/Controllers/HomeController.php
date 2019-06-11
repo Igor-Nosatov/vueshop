4bd81b19124551618326115a23f93724475460e7
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:52:05 +0300

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,22 +1,18 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Repositories\HomeRepository;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     protected $product;
-
     public function __construct(HomeRepository $product)
     {
-        $this->product = $product;
+       $this->product = $product;
     }
-
     public function index()
-	  {
-	    	$data['products'] = $this->product->getProduct();
-		    return response()->json($data, 200);
-	   }
+	{
+	    $data['products'] = $this->product->getProduct();
+		return response()->json($data, 200);
+	}
 }
