cdb1b430ff57c694c224e21287220312edb8935e
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:40:44 +0300

    Update CategoryRepository.php

--- app/Repositories/CategoryRepository.php
+++ app/Repositories/CategoryRepository.php
@@ -1,18 +1,14 @@
 <?php
-
 namespace App\Repositories;
-
 use App\Category;
 
-class CategoryRepository
-{
-  public function __construct(Category $category)
-    {
-      $this->category = $category;
+class CategoryRepository {
+    
+    public function __construct(Category $category) {
+        $this->category = $category;
     }
-
-  public function getCategory()
-   {
-      return $this->category->get();
-   }
+    public function getCategory() {
+        return $this->category->get();
+    }
 }
+
