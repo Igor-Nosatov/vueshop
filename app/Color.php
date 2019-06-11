093c3c77cb6eca83b8fda55fbbebf13c09d433a9
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:32:16 +0300

    Update Color.php

--- app/Color.php
+++ app/Color.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 class Color extends Model
 {
   protected $fillable = ['name'];
-
   public function product()
   {
     return $this->hasMany('App\Product','color_id');
