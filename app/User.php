168b8cdec8a2866ae3c03cf7e5ac65bc0053112d
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:37:21 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -1,26 +1,18 @@
 <?php
+namespace App;
+use Laravel\Passport\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-    namespace App;
-
-    use Laravel\Passport\HasApiTokens;
-    use Illuminate\Notifications\Notifiable;
-    use Illuminate\Database\Eloquent\SoftDeletes;
-    use Illuminate\Foundation\Auth\User as Authenticatable;
-
-    class User extends Authenticatable
+class User extends Authenticatable {
+    
+    use Notifiable, SoftDeletes, HasApiTokens;
+    
+    protected $fillable = ['name', 'email', 'password', ];
+    protected $hidden = ['password', 'remember_token', ];
+    public function orders() 
     {
-        use Notifiable, SoftDeletes, HasApiTokens;
-
-        protected $fillable = [
-            'name', 'email', 'password',
-        ];
-
-        protected $hidden = [
-            'password', 'remember_token',
-        ];
-
-        public function orders()
-        {
-            return $this->hasMany(Order::class);
-        }
+        return $this->hasMany(Order::class);
     }
+}
