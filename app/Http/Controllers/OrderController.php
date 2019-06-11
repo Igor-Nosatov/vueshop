0e8357423655e4422be5692032fbe2600e4b1a21
Igor Nosatov <sinevor13@gmail.com>
2019-06-11 08:52:30 +0300

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1,31 +1,25 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Repositories\OrderRepository;
 
 class OrderController extends Controller
 {
     protected $order;
-
     public function __construct(OrderRepository $order)
     {
         $this->order = $order;
     }
-
     public function index()
     {
         $data['order'] = $this->order->getOrder();
         return response()->json($data, 200);
     }
-
     public function store(Request $request)
     {
       $order = $this->order->createOrder($request->all());
       return response()->json('success');
     }
-
     public function delete($id)
     {
         $order = $this->order->deleteOrder($id);
